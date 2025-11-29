1a39249319f8363ff7d889a7d3832124882778b3
tpranatipatra123-max <t.pranatipatra123@gmail.com>
2025-11-29 11:50:03 +0000

    Feat: add, database

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
+# Starts the API server and initializes the database
+
 from http.server import HTTPServer
 from router import StudentRouter
 from database.connection import init_database
 
-
 def run_server(port=8000):
-    
-
+    init_database()
     server = HTTPServer(("", port), StudentRouter)
-    print(f"🚀 server running at http://localhost:{port}")
+    print(f"🚀 server running at http://localhost:8000")
     server.serve_forever()
 
-    if __name__ == "__main__":
-        run_server()
+if __name__ == "__main__":
+    run_server()
--- router.py
+++ router.py
@@ -4,6 +4,9 @@ from urllib.parse import urlparse
 
 from controllers.students import (
     get_all_students
+    , get_student
+    , create_student
+    
    
 )
 
@@ -26,9 +29,22 @@ class StudentRouter(BaseHTTPRequestHandler):
        
         if path == "/api/students":
             return get_all_students(self)
-
+        
+         # API: Get student by ID
+        if path.startswith("/api/students/"):
+                student_id = int(path.split("/")[-1])
+                return get_student(self, student_id)
         
         return send_404(self)
+           
+            
+    def do_POST(self):
+        if self.path == "/api/students":
+            return create_student(self)
+        return send_404(self)
+
+
+        
 
     
     
